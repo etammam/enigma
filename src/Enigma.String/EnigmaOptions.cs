f6cfc3246b01163262a8227086f8345026381961
Eslam M. Tammam <eslamtammam@hotmail.com>
2022-04-15 18:15:00 +0200

    #FOUNDATION: create a sample project to encrypt and decrypt a simple string based on symmetric key.

---
+++ src/Enigma.String/EnigmaOptions.cs
@@ -0,0 +1,17 @@
+﻿namespace Enigma.String
+{
+    public class EnigmaOptions
+    {
+        public EnigmaOptions()
+        {
+
+        }
+
+        public EnigmaOptions(string symmetricKey)
+        {
+            SymmetricKey = symmetricKey;
+        }
+
+        public string SymmetricKey { get; set; } = null!;
+    }
+}
